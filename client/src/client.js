791831bfd83ab1873dbdb520eba32604419946bc
emrekacan <emrekacan@gmail.com>
2021-03-07 10:48:40 +0300

    working on course part #1

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,31 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import {ApolloClient} from 'apollo-client'
+import {InMemoryCache} from 'apollo-cache-inmemory'
+import {HttpLink} from 'apollo-link-http'
+import {ApolloLink} from 'apollo-link'
+import {setContext} from 'apollo-link-context'
 
 /**
  * Create a new apollo client and export as default
  */
+const httpLink = new HttpLink({uri: 'http://localhost:4000/'})
+
+const delay = setContext(request => new Promise((success, fail)=> {
+    setTimeout(()=> {
+        success()
+    }, 800)
+
+}))
+
+const link = ApolloLink.from([
+    delay,
+    httpLink
+])
+
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+    link,
+    cache
+})
+
+export default client
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,39 +1,82 @@
 import React, {useState} from 'react'
 import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
+import {useQuery, useMutation} from '@apollo/react-hooks'
 import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+    query AllPets {
+        pets {
+            id
+            name
+            type
+            img
+        }
+    }
+`
 
-export default function Pets () {
-  const [modal, setModal] = useState(false)
+const NEW_PAT = gql`
+    mutation createAPet ($newPet: NewPetInput!) {
+        addPet(input: $newPet){
+            id
+            type
+            name
+            img
+        }
+    }
+`
 
+export default function Pets() {
+    const [modal, setModal] = useState(false)
+    const {data, loading, error} = useQuery(ALL_PETS)
+    const [createPet, newPet] = useMutation(NEW_PAT, {
+        update(cache, {data: {addPet}}) {
+                const data = cache.readQuery({query: ALL_PETS})
+                cache.writeQuery({
+                    query: ALL_PETS,
+                    data: { pets: [addPet, ...data.pets]}
+                })
+        }
+    })
 
-  const onSubmit = input => {
-    setModal(false)
-  }
-  
-  if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
-  }
+    const onSubmit = input => {
+        createPet({
+            variables: {
+                newPet: input
+            }
+        })
+        setModal(false)
+    }
 
-  return (
-    <div className="page pets-page">
-      <section>
-        <div className="row betwee-xs middle-xs">
-          <div className="col-xs-10">
-            <h1>Pets</h1>
-          </div>
+    if (modal) {
+        return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)}/>
+    }
 
-          <div className="col-xs-2">
-            <button onClick={() => setModal(true)}>new pet</button>
-          </div>
+    if (loading || newPet.loading) {
+        return <Loader/>
+    }
+
+    if (error || newPet.error) {
+        return <p>error</p>
+    }
+
+    return (
+        <div className="page pets-page">
+            <section>
+                <div className="row betwee-xs middle-xs">
+                    <div className="col-xs-10">
+                        <h1>Pets</h1>
+                    </div>
+
+                    <div className="col-xs-2">
+                        <button onClick={() => setModal(true)}>new pet</button>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <PetsList pets={data.pets}/>
+            </section>
         </div>
-      </section>
-      <section>
-        <PetsList />
-      </section>
-    </div>
-  )
+    )
 }
